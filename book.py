ae2d70d208965d66369e5bcc7697be902464987d
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2019-08-06 13:47:22 +0100

    Started creating a class to make the code cleaner and tidier

--- book.py
+++ book.py
@@ -1,4 +1,26 @@
-fileName = '.book'
+
+class Book():
+
+    def __init__( self, fileName ):
+        self.holdData = []
+
+        try:                # In case the file is already created
+            with open( fileName ) as fbuf:
+                contents = fbuf.readlines()
+                contents = [ x.strip() for x in contents ]
+                # Hold the data into holdData
+                auxHold = []
+                for line in contents:
+                    auxHold.append( line )
+                    if line == '--':
+                        self.holdData.append( auxHold )
+                        auxHold = []
+                # appData( 'prueba.prueba', self.holdData, 'Esto es lo que se debe de hacer' )
+                # finData( 'prueba.prueba', self.holdData )
+        except IOError:     # In case the file does not exist
+            with open( fileName, 'w' ) as fbuf:
+                fbuf.write( 'prueba.prueba' + '\n' )
+                fbuf.write( '--' )
 
 def checkEx( nameFile, holdData ):
     # Error is raised in case the file does not exist in file
@@ -12,7 +34,7 @@ def finData( nameFile, holdData ):
     for group in holdData:
         if nameFile in group:
             for elem in group:
-                print( elem )
+                print( elem ) if elem !=  '--' else None
 
 def appData( nameFile, holdData, comment ):
     # Append data to the last position in the group
@@ -22,25 +44,34 @@ def appData( nameFile, holdData, comment ):
             holdData[i].append( comment )
             holdData[i].append( '--' )
 
-holdData = []
-try:                # In case the file is already created
-    with open( fileName ) as fbuf:
-        contents = fbuf.readlines()
-        contents = [ x.strip() for x in contents ]
-        # Hold the data into holdData
-        auxHold = []
-        for line in contents:
-            auxHold.append( line )
-            if line == '--':
-                holdData.append( auxHold )
-                auxHold = []
-        appData( 'prueba.prueba', holdData, 'Esto es lo que se debe de hacer' )
-        finData( 'prueba.prueba', holdData )
 
+# holdData = []
+# try:                # In case the file is already created
+#     with open( fileName ) as fbuf:
+#         contents = fbuf.readlines()
+#         contents = [ x.strip() for x in contents ]
+#         # Hold the data into holdData
+#         auxHold = []
+#         for line in contents:
+#             auxHold.append( line )
+#             if line == '--':
+#                 holdData.append( auxHold )
+#                 auxHold = []
+#         appData( 'prueba.prueba', holdData, 'Esto es lo que se debe de hacer' )
+#         finData( 'prueba.prueba', holdData )
+#
+#
+# except IOError:     # In case the file does not exist
+#     with open( fileName, 'w' ) as fbuf:
+#         fbuf.write( 'prueba.prueba' + '\n' )
+#         fbuf.write( '--' )
 
-except IOError:     # In case the file does not exist
-    with open( fileName, 'w' ) as fbuf:
-        fbuf.write( 'prueba.prueba' + '\n' )
-        fbuf.write( '--' )
+if __name__ == '__main__':
+
+    # File to save the data
+    fileName = '.book'
+
+    # Create a Book object that will manage the IO
+    book = Book( fileName )
 
 
